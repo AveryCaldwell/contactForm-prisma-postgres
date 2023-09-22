57ceba5180bc5af57d2ded0429ac1d415f55e076
Avery <averycaldwell7@gmail.com>
2023-09-21 19:58:22 -0700

    still debugging, but server running successful

--- index.ts
+++ index.ts
@@ -1,14 +1,15 @@
 import express from "express";
-import contactFormRoute from "./routes/contactForm.js";
+import contactFormRoute from "./src/routes/contactForm";
 
 import { PrismaClient } from "@prisma/client";
 
-const router = express.Router();
 const prisma = new PrismaClient();
 
-// ... other middleware and configurations ...
 const app = express();
+
+// Middleware to parse JSON data
 app.use(express.json());
+
 // Mount the contact form route
 app.use("/contact", contactFormRoute);
 
@@ -17,5 +18,3 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
 	console.log(`Server is running on port ${port}`);
 });
-
-export default router;
